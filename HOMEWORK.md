d88bd2c44d66460c4ca4f1cf1b637b6167e57f29
JasontreksC <jasontreks@outlook.com>
2026-07-07 17:58:01 +0900

    homework: 숙제

---
+++ HOMEWORK.md
@@ -0,0 +1,9 @@
+########################
+# <파이썬 스터디 5회차 숙제>
+## 숙제 내용
+- homework_wjs를 참고하여, 임베딩 객체를 만들고 FAISS 벡터 DB를 만드는 방법을 복기합니다.
+- homework_base.py의 sentences 리스트에 원하는 문장들을 추가합니다.
+- 본인의 이름 이니셜로 새 파이썬 파일을 만듭니다. (homework_xxx.py)
+- sentences, get_dementions()를 import하여 활용해 코딩합니다.
+- sentences에 추가한 문장들 중 하나가 검색되도록 query_text를 작성하고 유사도 검색을 테스트합니다.
+- 유사도 검색 수행 결과를 주석문으로 추가합니다.
--- homework_wjs.py
+++ homework_wjs.py
@@ -24,7 +24,7 @@ faiss_vdb = FAISS(
 # 벡터 DB에 문장 추가 -> (임베딩 자동 수행)
 faiss_vdb.add_texts(sentences)
 # 유사도 검색 수행
-query_text = "내 스마트폰 화면이 부서졌습니다." # 검색할 문장
+query_text = "아침마다 조깅을 하는 습관은 좋은 습관이다." # 검색할 문장
 results = faiss_vdb.similarity_search_with_score(query_text, 3)
 # 검색 결과를 출력
 for i, r in enumerate(results):
@@ -32,3 +32,13 @@ for i, r in enumerate(results):
     score = r[1]
     print(f"검색 결과 {i + 1}: {document.page_content}")
     print(f"유사도 점수 (코사인): {score}")
+
+## 검색 결과 ##
+"""
+검색 결과 1: 아침마다 조깅을 하고 나면 하루 종일 컨디션이 훨씬 좋아지는 걸 느낀다.
+유사도 점수 (코사인): 0.7456008791923523
+검색 결과 2: 매일 아침 달리기를 마치고 나면 몸 상태가 확실히 좋아지는 게 느껴진다.
+유사도 점수 (코사인): 0.5916985869407654
+검색 결과 3: 새로 산 노트북은 배터리가 하루 종일 가고 발열도 거의 없어서 만족스럽다.
+유사도 점수 (코사인): 0.28454092144966125
+"""
